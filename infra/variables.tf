b2271ea6be7ad019b5bebda30f093429b6cdc898
Felipe Correia <correia.felipe@hotmail.com>
2025-08-29 19:58:57 -0300

    first

---
+++ infra/variables.tf
@@ -0,0 +1,26 @@
+variable "region"  { 
+    type = string 
+    default = "us-east-1" 
+}
+variable "incoming_bucket_name" { 
+    type = string 
+}
+
+variable "glue_job_a" { 
+    type = string 
+    default = "etapa2" 
+}
+variable "glue_job_b" { 
+    type = string 
+    default = "iceberglab-v1-tblproperties" 
+}
+
+
+variable "project" { 
+    type = string 
+    default = "rafa-infra-stf" 
+}
+variable "env"     { 
+    type = string 
+    default = "dev" 
+    }
